e6326e00775d915c3e6087158d6a16a43b004688
HimaVarshitha7 <231FA04A55@gmail.com>
2026-03-14 22:16:29 +0530

    Update backend API to HuggingFace

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // src/config.js
 
-export const API_BASE_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-    ? "http://127.0.0.1:5000" 
-    : "https://plagirism-backend.onrender.com";
+export const API_BASE_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://127.0.0.1:5000"
+    : "https://huggingface.co/spaces/HimaVarshitha/plagiarism-checker/proxy/7860";
