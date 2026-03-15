58c55e3dd3ca1aba046010e7874bc70c9ad9e71d
HimaVarshitha7 <231FA04A55@gmail.com>
2026-03-15 16:11:56 +0530

    commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
-// src/config.js
-
 export const API_BASE_URL =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
+  window.location.hostname === "localhost"
     ? "http://127.0.0.1:5000"
-    : "https://huggingface.co/spaces/HimaVarshitha/plagiarism-checker/proxy/7860";
+    : "https://your-railway-url.up.railway.app";
